a569c6678b1ace23aeb190c58521a39553a804c8
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-01-20 17:39:18 +0900

    updated

---
+++ src/components/InvoiceCode.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-card>
+    <v-container
+      fluid
+      style="min-height: 0;"
+      grid-list-lg
+      >
+      <v-flex xs12 sm6 offset-sm3>
+        <v-layout wrap column>
+          <v-flex>
+            <v-card class="invoice-container">
+              <div ref="invoiceData">
+                <v-flex>
+                  <h3 class="label-service">
+                    {{ serviceName }}
+                  </h3>
+                  <div>
+                    <qriously v-model="qrValue" :size="qrSize" :foreground="qrColor"/>
+                  </div>
+                  <table border="0" class="table__list">
+                      <tr>
+                        <td width="20%" class="table__key">コイン数</td>
+                        <td width="80%" class="table__value">{{ price }}</td>
+                      </tr>
+                      <tr>
+                        <td width="20%" class="table__key">利用条件</td>
+                        <td width="80%" class="table__text">{{ termsOfUse }}</td>
+                      </tr>
+                  </table>
+                </v-flex>
+                <v-flex>
+                  <p class="label-howtoreceive">{{ howtoReceive }}</p>
+                  <span class="label-event">{{ eventName }}</span>
+                </v-flex>
+              </div>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-container>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class InvoiceCode extends Vue {
+  private serviceName: string = 'ケンタッキー'
+  private price: string = '100 marine'
+  private termsOfUse: string = '１品以上ご注文いただいた方'
+  private howtoReceive: string = 'お越しの際にアプリを提示してください'
+  private eventName: string = '居酒屋 宮古島'
+  private qrValue: string = 'aiueo'
+  private qrSize: number = 300
+
+  // QRの色を設定
+  private qrColor: string = 'white'
+}
+</script>
+
+<!-- デザイン -->
+<style lang="stylus" scoped>
+@import '../assets/main.styl'
+
+p
+  font-size 14px
+
+// 請求書の背景
+.invoice-container
+  background blue
+
+// 請求書名
+.label-service
+  color white
+  margin 10px
+
+.table
+  &__list
+    margin 0 auto
+
+  // 左のコメント
+  &__key
+    font-size 14px
+    text-align center
+    color white
+    padding 8px 2px
+
+  // コイン
+  &__value
+    font-size 20px
+    font-weight bold
+    text-align center
+    color white
+    padding 8px
+
+  // 利用条件
+  &__text
+    font-size 14px
+    text-align center
+    color white
+    padding 8px
+
+// お受け取り
+.label-howtoreceive
+  color white
+
+// 加盟店
+.label-event
+  display block
+  font-size 14px
+  text-align right 
+  color white
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,38 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VueQriously from 'vue-qriously'
+import Vuetify from 'vuetify'
+import colors from 'vuetify/es5/util/colors'
 
 Vue.config.productionTip = false;
+
+Vue.use(VueQriously)
+Vue.use(Vuetify, {
+  theme: {
+    original: colors.purple.base,
+    theme: '#5982EE',
+    background: '#ffffff',
+    whiteCloud: '#F8F9FB',
+    grayTheme: '#79838C',
+    blueTheme: '#5982EE',
+    sky: '#A2DCED',
+    sea: '#698FF0',
+    blackFlat: '#202223',
+    twitter: '#00aced',
+    facebook: '#305097',
+    line: '#5ae628',
+    gold: '#FFD700',
+    goldenrod: '#DAA520',
+    red: '#F26964',
+    error: '#F26964',
+    succcess: '#698FF0',
+  },
+  options: {
+    themeVariations: ['original', 'secondary'],
+  },
+})
+
 
 new Vue({
   router,
